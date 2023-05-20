84350b20551c31f480e7a22c0fd29790ee727b85
Halyna Stepanenko <galinavikst@gmail.com>
2023-05-20 21:17:59 +0200

    fear: implement contributors cards and functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", './index.html'],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'main': '#F1DCFC',
-      'btn-cl': '#FF66C4',
-      'accent-cl': "#FF27AC",
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      main: "#F1DCFC",
+      "btn-cl": "#FF66C4",
+      "accent-cl": "#FF27AC",
     },
     extend: {},
   },
   plugins: [],
-}
-
-
-
-
+};
