cd83ef4d2e718af84b48b2fd52a6d3ec1fe9f3ab
Halyna Stepanenko <galinavikst@gmail.com>
2023-08-07 17:12:25 +0300

    implement localization

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import { translation } from "./localization";
 
+import AlpineI18n from "alpinejs-i18n";
+import Alpine from "alpinejs";
+
+let locale = "uk"; //initial language
+
+document.addEventListener("alpine-i18n:ready", function () {
+  window.AlpineI18n.create(locale, translation);
+});
+
+Alpine.plugin(AlpineI18n);
+Alpine.start();
